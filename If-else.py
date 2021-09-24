b6763891eee276994ea46963b739b996f0759b04
SabbirAhmed81 <90913694+SabbirAhmed81@users.noreply.github.com>
2021-09-24 11:42:15 +0600

    Create If-else.py

---
+++ If-else.py
@@ -0,0 +1,16 @@
+a=21
+if(a==21):
+	print("sabbir")
+	print("class 8")
+	print("vill. nogapara")
+	print("year 15")
+if(a==17):
+	print("tanvir")
+	print("class 7")
+	print("vill. dhaka")
+	print("year 14")
+if(a==71):
+	print("niver")
+	print("class 9")
+	print("vill. sylhet")
+	print("year 16")
